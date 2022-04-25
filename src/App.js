a557d9f32f31aa49ff03c3e8220872c0f515af38
Chudi <chudionubogu@yahoo.com>
2022-04-25 10:59:32 -0500

    Routes are set up, time to work on global authorization for front end

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
-import { Home } from "./components";
+import { Home, Cart, Inventory, Login, Logout, Register } from "./components";
 
 function App() {
+  const token = false;
   return (
     <div>
       <header>
         <h3>Royal Flush Homepage</h3>
         <Link to="/">Home</Link>
+        <Link to="/Inventory">Inventory</Link>
+        {token ? (
+          <>
+            <Link to="/Logout">Logout</Link>
+            <Link to="/cart">Cart</Link>
+          </>
+        ) : (
+          <>
+            <Link to="/Login">Login</Link>
+            <Link to="/signup">Sign Up</Link>
+          </>
+        )}
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/checkout" element={<Cart />} />
+        <Route path="/inventory" element={<Inventory />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/signup" element={<Register />} />
       </Routes>
     </div>
   );
---
+++ src/components/Register.js
@@ -0,0 +1,11 @@
+import React, { memo } from "react";
+
+const Register = memo(() => {
+  return (
+    <div>
+      <h1>yo</h1>
+    </div>
+  );
+});
+
+export default Register;
