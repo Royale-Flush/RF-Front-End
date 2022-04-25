4f3a1b7e64b9b0ead560513235c244b52872ecf1
coryla <cory.lance.anderson@gmail.com>
2022-04-25 13:15:42 -0500

    first commit logout

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,10 +1,34 @@
-import React from "react";
+// import React from "react";
 
-const Logout = () => {
+// const Logout = () => {
+//   return (
+//     <div>
+//       <h1>hello</h1>
+//     </div>
+//   );
+// };
+
+// export default Logout;
+
+import React from "react";
+import { useAuth } from "../hooks/useAuth";
+
+const Logout = ({ setIsLoggedIn, isLoggedIn }) => {
+  const { token, setToken } = useAuth;
+  const logoutUser = () => {
+    localStorage.removeItem("token");
+    setIsLoggedIn(false);
+    setToken(localStorage.removeitem("token"));
+  };
+
   return (
-    <div>
-      <h1>hello</h1>
-    </div>
+    <form
+      onSubmit={() => {
+        logoutUser();
+      }}
+    >
+      <button type="submit">Logout</button>
+    </form>
   );
 };
 
